8d460221600e47400530addbf65bbc66d27c98a0
Splimis <splimis@gmail.com>
2017-11-30 09:55:46 -0500

    boiler plate the server

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express(); 
+
+var PORT = process.env.PORT || 3000; 
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+// Serve static content for the app from the 'public' directory
+app.use("/public", express.static(__dirname + '/public'));
+// Serve static content from a possible scripts 'file'
+app.use("/scripts", express.static(__dirname + '/scripts'));
+
+// require('./App/routing/apiRoutes.js')(app); 
+require('./routes/htmlRoutes.js')(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
